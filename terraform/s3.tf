3cbc74ecacb18d2a597661a237696d75edc0e802
Joshua Mottaz <gerhalt@gmail.com>
2017-08-22 20:19:33 -0700

    S3 Terraform Config
    Added basic bucket definition and lifecycle rules for managing tagged
    backups that aren't current.

---
+++ terraform/s3.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "minecraft" {
+  bucket = "${var.minecraft["bucket_name"]}"
+  acl    = "private"
+
+  lifecycle_rule {
+    id      = "general"
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+
+  lifecycle_rule {
+    id      = "backup pruning"
+    enabled = true
+
+    tags {
+      "backup"    = "old"
+    }
+
+    expiration {
+      days = 3
+    }
+  }
+}
