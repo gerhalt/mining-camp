e3eda6558102b590bab780936dbd863a4f0bd6ab
Joshua Mottaz <gerhalt@gmail.com>
2017-08-25 22:27:16 -0700

    First stab at bootstrapping script

--- utilities/bootstrap.sh
+++ utilities/bootstrap.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 #
 # Bootstraps the minecraft server into a tmux session for easy access
-echo "BOOTSTRAPPING MINECRAFT"
+#
+# ./bootstrap server_path
+
+if [ $# -eq 0 ]; then
+    echo "First parameter should be the minecraft server root directory"
+    exit 1
+fi
+
+# Change to the server root
+cd $1
+
+# Create a detached tmux session named "minecraft" and launch the server in it
+session_name=minecraft
+tmux new-session -s ${session_name} -d
+pane=${session_name}:0.0
+tmux send-keys -t "$pane" './ServerStart.sh' C-m
+
+echo "Minecraft server launched, attach to it by running:"
+echo "tmux attach-session -t ${session_name}"
