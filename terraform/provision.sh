fa5439e1eacadd2dce9fd7d7a9b5ca3b728ef48b
Josh <gerhalt@gmail.com>
2020-05-16 22:32:17 -0600

    Minor comments

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
+# Bootstrap script template to be run on the instance immediately after startup
 
 exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
 
