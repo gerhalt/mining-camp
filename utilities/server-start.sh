9e696537f1003f0a4df1812e34de52bb61043c75
Josh <gerhalt@gmail.com>
2020-05-25 15:35:02 -0600

    Add generic server-start.sh script and update README

---
+++ utilities/server-start.sh
@@ -0,0 +1,5 @@
+# Solely responsible for bootstrapping the minecraft server
+#
+# You should modify this and bundle it into the root directory of your server
+# archive
+java -Xmx4G -Xms4G -jar minecraft_server.1.15.2.jar nogui
